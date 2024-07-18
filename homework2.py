0318230c70868d614a160f680dd79ae7b1197a90
lielrad <127157176+lielrad@users.noreply.github.com>
2024-07-18 20:49:33 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_tests = 12
+hours_spend = 1.5
+name_of_course = 'Python'
+average_time_for_test = hours_spend / number_of_tests
+print('Курс: '+ name_of_course + ',' , 'всего задач: '+ str(number_of_tests) + ',' , 'затрачено часов: '+ str(hours_spend) + ',' , 'среднее время выполнения: ' + str(average_time_for_test) + '.')
