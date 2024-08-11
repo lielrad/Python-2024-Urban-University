5e66a8a56a41b473c40d484d638e59541f9c97b7
lielrad <127157176+lielrad@users.noreply.github.com>
2024-08-11 21:32:43 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from true_math import true_divide as true_d
+from fake_math import fake_divide as fake_d
+
+
+result1 = fake_d(69, 3)
+result2 = fake_d(3, 0)
+result3 = true_d(49, 7)
+result4 = true_d(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
