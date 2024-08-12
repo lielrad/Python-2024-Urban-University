262334525edf602f03c6654a23c5fce389daf6f1
lielrad <127157176+lielrad@users.noreply.github.com>
2024-08-12 16:59:50 +0300

    Add files via upload

---
+++ module_4_1/fake_math.py
@@ -0,0 +1,5 @@
+def fake_divide(a, b):
+    if b == 0:
+        return 'Ошибка'
+    else:
+        return a / b
---
+++ module_4_1/true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+def true_divide(a, b):
+    if b == 0:
+        return inf
+    else:
+        return a / b
