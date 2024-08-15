dc28e9b0103e061c797838d32ba0a2b296ab276f
lielrad <127157176+lielrad@users.noreply.github.com>
2024-08-15 15:48:55 +0300

    Delete module_4_1/module_4_2.py

--- module_4_1/module_4_2.py
+++
@@ -1,8 +0,0 @@
-def test_function():
-    def inner_function():
-        print('Я в области видимости функции test_function')
-    inner_function()
-
-test_function()   # Я в области видимости функции test_function
-inner_function()  # NameError: name 'inner_function' is not defined.
-# Did you mean: 'test_function'?
