b0fe71516a8825f092ba9d22bdc637a96d443d5c
lielrad <127157176+lielrad@users.noreply.github.com>
2024-08-21 15:32:57 +0300

    Add files via upload

--- Module_5/module_5_1.py
+++ Module_5/module_5_1.py
@@ -5,16 +5,13 @@ class House():
 
     def go_to(self, new_floor):
         if new_floor >= 1 and new_floor <= self.number_of_floors:
-            print(new_floor)
+            for i in range(new_floor):
+                print(i+1)
         else:
             print('Такого этажа не существует')
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 
-h2.go_to(1)
-h2.go_to(2)
-h1.go_to(3)
-h1.go_to(4)
 h1.go_to(5)
 h2.go_to(10)
